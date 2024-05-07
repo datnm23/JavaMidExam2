86e4118229bf6dc42ce30afdc9ba5501ac944a43
datnm23 <147710347+datnm23@users.noreply.github.com>
2024-05-07 14:50:40 +0700

    update menu

--- MidExam2/src/Menu.java
+++ MidExam2/src/Menu.java
@@ -1,12 +1,16 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Menu {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String REGEX_EMAIL_PATTERN = "^[a-zA-Z][\\\\w-]+@([\\\\w]+\\\\.[\\\\w]+|[\\\\w]+\\\\.[\\\\w]{2,}\\\\.[\\\\w]{2,})$";
+    private static final String REGEX_PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{8,15}$";
+    private static final UserService USERSERVICE = new UserService();
 
     public void displayMenuHome() throws IOException {
-        UserService userService = new UserService();
         System.out.println("1. Đăng nhập");
         System.out.println("2. Đăng ký");
         System.out.println("3. Quên mật khẩu");
@@ -15,16 +19,15 @@ public class Menu {
         switch (choice) {
             case 1:
                 System.out.println("Đã chọn 1:");
-
-                userService.login();
+                login();
                 break;
             case 2:
                 System.out.println("Đã chọn 2:");
-                userService.register();
+                register();
                 break;
             case 3:
                 System.out.println("Đã chọn 3:");
-                userService.forgotPassword();
+                forgotPassword();
                 break;
             default:
                 System.out.println("Invalid choice");
@@ -41,20 +44,22 @@ public class Menu {
         System.out.println("Please chooose : ");
 
         int choice2 = Integer.parseInt(SCANNER.nextLine());
-        UserService userService = new UserService();
         switch (choice2) {
 
             case 1:
                 System.out.println("Da chon 1 : ");
-                userService.changeUserName(userLogin);
+                changeUserName(userLogin);
+                System.out.println("Thay đổi Username thành công ! UserName mới của bạn là :" + userLogin.getUserName());
                 break;
             case 2:
                 System.out.println("Da chon 2 : ");
-                userService.changeUserEmail(userLogin);
+                changeUserEmail(userLogin);
+                System.out.println("Thay đổi Email thành công ! Email mới của bạn là :" + userLogin.getUserEmail());
                 break;
             case 3:
                 System.out.println("Da chon 3 : ");
-                userService.changeUserPassword(userLogin);
+                changeUserPassword(userLogin);
+                System.out.println("Thay đổi Password thành công ! Password mới của bạn là :" + userLogin.getUserPassword());
                 break;
             case 4:
                 Menu menu = new Menu();
@@ -67,5 +72,103 @@ public class Menu {
         }
     }
 
+    public void login() throws IOException {
+        Menu menu = new Menu();
+        User userLogin = null;
+
+        while (userLogin == null) {
+            System.out.println("Email đăng nhập: ");
+            String email = SCANNER.nextLine();
+            System.out.println("Mật khẩu: ");
+            String password = SCANNER.nextLine();
+            userLogin = USERSERVICE.checkUser(email, password);
+            if (userLogin != null) {
+                System.out.println("Đăng nhập thành công!");
+            } else {
+                System.out.println("Tài khoản hoặc mật khẩu không chính xác");
+            }
+        }
+        menu.displayMenuLogin(userLogin);
+    }
+
+    public void register() throws IOException {
+        ArrayList<User> users = USERSERVICE.getUsers();
+        Menu menu = new Menu();
+        User newUser = new User();
+        changeUserName(newUser);
+        changeUserEmail(newUser);
+        changeUserPassword(newUser);
+        System.out.println("Chúc mừng ! Bạn đã tạo tài khoản thành công. Xin mời đăng nhập:");
+        users.add(newUser);
+        menu.displayMenuHome();
+    }
+
+    public void forgotPassword() throws IOException {
+        System.out.println("\n********* ĐỔI MẬT KHẨU *********");
+        ArrayList<User> users = USERSERVICE.getUsers();
+        boolean checkInvalidEmail = false;
+        while (!checkInvalidEmail) {
+            System.out.println("Nhập địa chỉ mail của bạn: ");
+            String email = SCANNER.nextLine();
+            checkInvalidEmail = true;
+            if (USERSERVICE.checkEmail(email)) {
+                for (User user : users) {
+                    if (user.getUserEmail().equalsIgnoreCase(email)) {
+                        changeUserPassword(user);
+                        Menu menu = new Menu();
+                        menu.displayMenuHome();
+                        break;
+                    }
+                }
+            } else {
+                checkInvalidEmail = false;
+                System.out.println("Email không tồn tại !");
+            }
+        }
+    }
+
+    public void changeUserName(User userLogin) {
+            boolean checkUsername = false;
+            while (!checkUsername) {
+                System.out.println("Nhập Username mới");
+                String newUserName = SCANNER.nextLine();
+                if (USERSERVICE.checkUsername(newUserName)) {
+                    System.out.println("Username đã tồn tại ! Vui lòng chọn Username khác : ");
+                } else {
+                    checkUsername = true;
+                    userLogin.setUserName(newUserName);
+                }
+            }
+    }
+    public void changeUserEmail(User userLogin) {
+        boolean checkEmail = false;
+        while (!checkEmail) {
+            System.out.println("Nhập Email mới");
+            String newUserEmail = SCANNER.nextLine();
+            if (USERSERVICE.checkEmail(newUserEmail)) {
+                System.out.println("Email đã tồn tại ! Nhập email khác:");
+            } else if (Pattern.matches(REGEX_EMAIL_PATTERN, newUserEmail)) {
+                System.out.println("Định dạng email không đúng!");
+            } else {
+                checkEmail = true;
+                userLogin.setUserEmail(newUserEmail);
+            }
+        }
+    }
+
+    public void changeUserPassword(User userLogin) {
+        boolean checkPassword = false;
+        System.out.println("Password must contain at least one digit [0-9].at least one lowercase Latin character [a-z].at least one uppercase Latin character [A-Z].at least one special character.a length of at least 8 characters and a maximum of 20 characters ");
+        while (!checkPassword) {
+            System.out.println("Nhập Password mới");
+            String newUserPassword = SCANNER.nextLine();
+            if (Pattern.matches(REGEX_PASSWORD_PATTERN, newUserPassword)) {
+                System.out.println("Định dạng password không đúng!");
+            } else {
+                checkPassword = true;
+                userLogin.setUserPassword(newUserPassword);
+            }
+        }
+    }
 }
 
--- MidExam2/src/UserService.java
+++ MidExam2/src/UserService.java
@@ -4,18 +4,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 public class UserService {
-    private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String REGEX_EMAIL_PATTERN = "^[a-zA-Z][\\\\w-]+@([\\\\w]+\\\\.[\\\\w]+|[\\\\w]+\\\\.[\\\\w]{2,}\\\\.[\\\\w]{2,})$";
-    private static final String REGEX_PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
-
     private ArrayList<User> users;
 
     public UserService() {
@@ -30,6 +24,10 @@ public class UserService {
         }
     }
 
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
     public void SyncFile() throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
@@ -41,10 +39,6 @@ public class UserService {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-    }
-
-    public ArrayList<User> getUsers() {
-        return users;
     }
 
     public User checkUser(String email, String password) {
@@ -76,164 +70,5 @@ public class UserService {
             }
         }
         return false;
-    }
-
-    public User login() throws IOException {
-        Menu menu = new Menu();
-        User userLogin = null;
-
-        while (userLogin == null) {
-            System.out.println("Email đăng nhập: ");
-            String email = SCANNER.nextLine();
-            System.out.println("Mật khẩu: ");
-            String password = SCANNER.nextLine();
-            userLogin = checkUser(email, password);
-            if (userLogin != null) {
-                System.out.println("Đăng nhập thành công!");
-            } else {
-                System.out.println("Tài khoản hoặc mật khẩu không chính xác");
-            }
-        }
-
-        menu.displayMenuLogin(userLogin);
-        return userLogin;
-    }
-
-    public void changeUserName(User userLogin) {
-        boolean checkUsername = false;
-        while (!checkUsername) {
-            System.out.println("Nhập Username mới");
-            String newUserName = SCANNER.nextLine();
-            if (checkUsername(newUserName)) {
-                System.out.println("Username đã tồn tại ! Vui lòng chọn Username khác : ");
-            } else {
-                checkUsername = true;
-                userLogin.setUserName(newUserName);
-            }
-        }
-        System.out.println("Thay đổi Username thành công ! UserName mới của bạn là :" + userLogin.getUserName());
-
-        for (User user : users) {
-            System.out.println(user.toString());
-        }
-    }
-
-    public void changeUserEmail(User userLogin) {
-        boolean checkEmail = false;
-        while (!checkEmail) {
-            System.out.println("Nhập Email mới");
-            String newUserEmail = SCANNER.nextLine();
-            if (checkEmail(newUserEmail)) {
-                System.out.println("Email đã tồn tại ! Nhập email khác:");
-            } else if (Pattern.matches(REGEX_EMAIL_PATTERN, newUserEmail)) {
-                System.out.println("Định dạng email không đúng!");
-            } else {
-                checkEmail = true;
-                userLogin.setUserEmail(newUserEmail);
-            }
-        }
-        System.out.println("Thay đổi Email thành công ! Email mới của bạn là :" + userLogin.getUserEmail());
-    }
-
-    public void changeUserPassword(User userLogin) {
-        boolean checkPassword = false;
-        System.out.println("Password must contain at least one digit [0-9].at least one lowercase Latin character [a-z].at least one uppercase Latin character [A-Z].at least one special character.a length of at least 8 characters and a maximum of 20 characters ");
-        while (!checkPassword) {
-            System.out.println("Nhập Password mới");
-            String newUserPassword = SCANNER.nextLine();
-            if (Pattern.matches(REGEX_PASSWORD_PATTERN, newUserPassword)) {
-                System.out.println("Định dạng password không đúng!");
-            } else {
-                checkPassword = true;
-                userLogin.setUserPassword(newUserPassword);
-            }
-        }
-        System.out.println("Thay đổi Password thành công ! Password mới của bạn là :" + userLogin.getUserPassword());
-    }
-
-    public void register() throws IOException {
-        System.out.println("Mời bạn nhập Username mới : ");
-        User newUser = new User();
-        newUser.setUserName(SCANNER.nextLine());
-        Menu menu = new Menu();
-        boolean checkInvalidEmail = true;
-
-        while (checkInvalidEmail) {
-            checkInvalidEmail = false;
-            for (User user : users) {
-                if (user.getUserName().equalsIgnoreCase(newUser.getUserName())) {
-                    System.out.println("Username đã tồn tại ! Vui lòng chọn Username khác : ");
-                    newUser.setUserName(SCANNER.nextLine());
-                    checkInvalidEmail = true;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("Mời bạn nhập Email mới : ");
-        newUser.setUserEmail(SCANNER.nextLine());
-        boolean notAccept = true;
-        while (notAccept) {
-            notAccept = false;
-            for (User user : users) {
-                if (user.getUserEmail().equalsIgnoreCase(newUser.getUserEmail())) {
-                    System.out.println("Email đã tồn tại ! Vui lòng chọn Email khác : ");
-                    newUser.setUserEmail(SCANNER.nextLine());
-                    notAccept = true;
-                    break;
-                }
-            }
-        }
-
-        notAccept = true;
-        while (notAccept) {
-            notAccept = false;
-            if (!newUser.getUserEmail().matches(REGEX_EMAIL_PATTERN)) {
-                System.out.println("Email không đúng định dạng !");
-                newUser.setUserEmail(SCANNER.nextLine());
-                System.out.println("Email của bạn" + newUser.getUserEmail());
-                notAccept = true;
-            }
-        }
-
-        System.out.println("Nhập mật khẩu: ");
-        newUser.setUserPassword(SCANNER.nextLine());
-        notAccept = true;
-        while (notAccept) {
-            notAccept = false;
-            if (!newUser.getUserPassword().matches(REGEX_PASSWORD_PATTERN)) {
-                System.out.println("Mật khẩu không hợp lệ: ");
-                newUser.setUserPassword(SCANNER.nextLine());
-                notAccept = true;
-            }
-        }
-        System.out.println("Chúc mừng ! Bạn đã tạo tài khoản thành công. Xin mời đăng nhập:");
-        users.add(newUser);
-        menu.displayMenuHome();
-    }
-
-
-    public void forgotPassword() throws IOException {
-        System.out.println("\n********* ĐỔI MẬT KHẨU *********");
-        boolean checkInvalidEmail = false;
-        while (!checkInvalidEmail) {
-            System.out.println("Nhập địa chỉ mail của bạn: ");
-            String email = SCANNER.nextLine();
-            checkInvalidEmail = true;
-            if (checkEmail(email)) {
-                for (User user : users) {
-                    if (user.getUserEmail().equalsIgnoreCase(email)) {
-                        changeUserPassword(user);
-                        Menu menu = new Menu();
-                        menu.displayMenuHome();
-                        break;
-                    }
-                }
-            } else {
-                checkInvalidEmail = false;
-                System.out.println("Email không tồn tại !");
-            }
-        }
-
     }
 }
